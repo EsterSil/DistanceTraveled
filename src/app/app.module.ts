4fac27824be449d853281889f99dfd730cd34479
estersil <lernichenko@phystech.edu>
2018-08-30 20:35:52 +0300

    Frontend Map with Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, TemplateRef} from '@angular/core';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {MapComponent} from './map/map.component';
+import {AgmCoreModule} from '@agm/core';
+import {MapContentComponent} from './map-content/map-content.component';
 
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    MapContentComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, HttpClientModule, CommonModule,
+    AgmCoreModule.forRoot({apiKey: 'AIzaSyB48P592kV2b7iq71QvnmfP66CF4uqUFZ8'}),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyB48P592kV2b7iq71QvnmfP66CF4uqUFZ8',
+      libraries: ['geometry']
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
