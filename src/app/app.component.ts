e647942c7e1b00e21349c6437251720d4add2ae7
estersil <lernichenko@phystech.edu>
2018-08-30 19:19:36 +0300

    DistanceTravelled FrontEnd Map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { KNOWN } from './mock-events';
+import {MapEventService} from './map-event.service';
+import {MapEvent} from './map-event';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'FrontMap';
+export class AppComponent implements OnInit{
+  knownEvents: MapEvent[];
+  eventMode = false;
+  constructor(private mapEventService: MapEventService) {
+  }
+  onEventsMode(): void {
+    this.eventMode = true;
+  }
+  getKnownEvents(): void {
+    this.mapEventService.getEvents().subscribe(mapEvents => this.knownEvents = mapEvents);
+  }
+
+  updData(): void {
+    this.getKnownEvents();
+  }
+  ngOnInit(): void {
+    this.getKnownEvents();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, TemplateRef } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+import { MapComponent } from './map/map.component';
+import { AgmCoreModule } from '@agm/core';
 import { AppComponent } from './app.component';
-
+import { EventConstructorComponent } from './event-constructor/event-constructor.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MapContentComponent } from './map-content/map-content.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    EventConstructorComponent,
+    MapContentComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, HttpClientModule,
+    AgmCoreModule.forRoot({apiKey: 'AIzaSyB48P592kV2b7iq71QvnmfP66CF4uqUFZ8'}),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyB48P592kV2b7iq71QvnmfP66CF4uqUFZ8',
+      libraries: ['geometry']
+    }),
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }),
   ],
   providers: [],
   bootstrap: [AppComponent]
